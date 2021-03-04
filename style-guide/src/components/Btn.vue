d97b755a0537c51dadba2596d3b93e0b2b786cc6
WhalenSITHS <38401651+WhalenSITHS@users.noreply.github.com>
2021-03-03 20:21:18 -0500

    playground

---
+++ style-guide/src/components/Btn.vue
@@ -0,0 +1,38 @@
+<template>
+  <button><slot></slot></button>
+</template>
+
+<script>
+export default {
+  name: "default-button",
+};
+</script>
+
+<style lang="scss">
+button {
+  height: 50px;
+  width: 500px;
+  border-radius: 10px;
+  font-size: 50px;
+  background-color: blue;
+  transition: all 0.3s;
+}
+button:hover {
+  background-color: red;
+}
+</style>
+<docs>
+This is my test button
+## Examples
+
+Test Button
+
+```jsx
+<default-button>
+  Sample Text
+</default-button>
+```
+
+    
+   
+</docs>
